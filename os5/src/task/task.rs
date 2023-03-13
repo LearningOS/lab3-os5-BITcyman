4c751a8dd7044f45ae20585cbb9ec6d6d29a9eb5
BITcyman <815207911@qq.com>
2023-03-13 09:29:45 +0000

    update

---
+++ os5/src/task/task.rs
@@ -0,0 +1,62 @@
+use super::{PidHandle, KernelStack, TaskContext};
+
+use crate::mm::{PhysPageNum, MemorySet};
+use crate::sync::UPSafeCell;
+
+
+pub struct TaskControlBlock {
+    pub pid: PidHandle,
+    pub kernel_stack: KernelStack,
+    inner: UPSafeCell<TaskControlBlockInner>,
+}
+
+impl TaskControlBlock {
+    pub fn inner_exclusive_access(&self) -> RefMut<'_, TaskControlBlockInner> {
+        self.inner.exclusive_access()
+    }
+    pub fn getpid(&self) -> usize {
+        self.pid.0
+    }
+    pub fn new(elf_data: &[u8]) -> Self {
+        //...
+    }
+    pub fn exec(&self, elf_data: &[u8]) {
+        //...
+    }
+    pub fn fork(self: &Arc<TaskControlBlock>) -> Arc<TaskControlBlock> {
+        //...
+    }
+}
+
+pub struct TaskControlBlockInner {
+    pub trap_cx_ppn: PhysPageNum,
+    pub base_size: usize,
+    pub task_cx: TaskContext,
+    pub task_status: TaskStatus,
+    pub memory_set: MemorySet,
+    pub parent: Option<Weak<TaskControlBlock>>,
+    pub children: Vec<Arc<TaskControlBlock>>,
+    pub exit_code: i32,
+}
+
+impl TaskControlBlockInner {
+    pub fn get_current_trap_cx(&self) -> &'static mut TrapContext {
+        self.trap_cx_ppn.get_mut()
+    }
+    pub fn get_user_token(&self) -> usize {
+        self.memory_set.token()
+    }
+    fn get_status(&self) -> TaskStatus {
+        self.task_status
+    }
+    pub fn is_zombie(&self) -> bool {
+        self.get_status() == TaskStatus::Zombie
+    }
+}
+
+pub enum TaskStatus {
+    UnInit,
+    Ready,
+    Running,
+    Zombie,
+}
